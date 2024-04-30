d5a25470a3d2376ce474333cf9c1f91c348f57ea
Beatriz Ceschini <biaceschini@gmail.com>
2024-04-30 15:50:59 -0300

    Person model

---
+++ infoManager/Models/Enums/StatusEnum.cs
@@ -0,0 +1,10 @@
+﻿namespace infoManager.Models.Enums
+{
+    public enum StatusEnum
+    {
+        Active = 0,
+        Disabled = 1,
+        Suspended = 2,
+        Pending = 3
+    }
+}
---
+++ infoManager/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿using infoManager.Models.Enums;
+
+namespace infoManager.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Cpf {  get; set; }
+        public DateOnly Birthday { get; set; }
+        public StatusEnum Status {  get; set; }
+        
+    }
+}
