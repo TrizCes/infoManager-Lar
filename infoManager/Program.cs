0ca268f1bba808b4088ba3b0c73bc08ebab3dce5
Beatriz Ceschini <biaceschini@gmail.com>
2024-05-11 23:56:17 -0300

    add cors

--- infoManager/Program.cs
+++ infoManager/Program.cs
@@ -49,6 +49,16 @@ builder.Services.AddAuthentication(x =>
 
 });
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.WithOrigins("https://localhost:5173")
+               .AllowAnyHeader()
+               .AllowAnyMethod();
+    });
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
